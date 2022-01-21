3a8849eac0dd88c4f3aec6f7f71e7276d9682501
phferreira <p.h.ferreirah@gmail.com>
2022-01-20 21:46:20 -0300

    Included home for micro_frontends archtecture

---
+++ lib/app/home_resolver.dart
@@ -0,0 +1,13 @@
+import 'package:core/app/micro_core_utils.dart';
+import 'package:core/app/microapp.dart';
+import 'package:home/app/presentation/home_page.dart';
+
+class HomeResolver implements MicroApp {
+  @override
+  String get appName => 'home';
+
+  @override
+  WidgetBuilderRoutes get routes => {
+        '/login': (context, args) => const HomePage(),
+      };
+}
